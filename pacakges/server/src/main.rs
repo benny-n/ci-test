82465143dc6bea09e0e4d296072feb6fe9d9521e
benny-n <benick12@gmail.com>
2021-11-28 18:05:32 +0200

    mod

--- pacakges/server/src/main.rs
+++ pacakges/server/src/main.rs
@@ -2,7 +2,18 @@ fn main() {
     println!("Hello, world!");
 }
 
-#[test]
-fn fail_test(){
-    assert!(false);
+#[cfg(test)]
+mod tests{
+
+    fn take_vec(_: &Vec<u8>){
+        println!("");
+    }
+
+    #[test]
+    fn fail_test(){
+        let x = if 5 == 5 { 5} else { 5 };
+        take_vec(&vec![]);
+        assert_eq!(x, 5);
+        assert!(false);
+    }
 }
