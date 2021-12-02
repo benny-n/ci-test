b04bc16951a9a3a799d375f0cbfb83e46d1744f3
benny-n <benick12@gmail.com>
2021-12-02 21:03:35 +0200

    more secret tests

--- pacakges/server/src/main.rs
+++ pacakges/server/src/main.rs
@@ -5,6 +5,7 @@ mod tests {
     fn env_test() {
         std::env::var("HEROKU_API_KEY").unwrap();
         assert_eq!(std::env::var("CARGO_TERM_COLOR").unwrap(), "always");
+        assert_eq!(std::env::var("SOMEVAR").unwrap(), "this_works");
     }
 }
 
