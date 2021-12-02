003dcc05efbac21f67ff1d6762a9312f6b65852a
benny-n <benick12@gmail.com>
2021-12-02 20:55:36 +0200

    try env

--- pacakges/server/src/main.rs
+++ pacakges/server/src/main.rs
@@ -1,23 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-}
 
-async fn what(){
-    todo!();
-}
 
 #[cfg(test)]
 mod tests {
 
-    fn take_vec(asd: &Vec<u8>) {
-        println!("{:#?}", asd);
-    }
-
     #[test]
-    fn fail_test() {
-        let x = if 5 == 5 { 5 } else { 5 };
-        take_vec(&vec![]);
-        assert_eq!(x, 5);
-        assert!(true);
+    fn env_test() {
+        println!("{:#?}", std::env::var("HEROKU_API_KEY"));
     }
 }
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
